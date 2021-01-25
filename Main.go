641f5efdb559c3b3ac77adecfd5af1c6db9501ee
mmxcrono <hnguyen.email@gmail.com>
2021-01-25 02:37:46 -0800

    Add language tests

--- Main.go
+++ Main.go
@@ -1,18 +1,42 @@
 package main
 import (
 	"fmt"
+	"strconv"
 )
 
 const englishHelloPrefix = "Hello, "
 
+const (
+	english = 1 << iota
+	spanish
+	french
+)
+
 // Hello returns welcome greeting
-func Hello(name string) string {
+func Hello(name string, language int) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language int) string {
+	switch language {
+	case spanish:
+		return "Hola, "
+	case french:
+		return "Bonjour, "
+	default:
+		return "Hello, "
+	}
 }
 
 func main() {
-	fmt.Println("Main")
+	allowedLanguages := english | spanish
+	language := french
+	
+	if language & allowedLanguages != 0 {
+		fmt.Println("Language allowed: " + strconv.Itoa(language))
+	}
 }
--- Main_test.go
+++ Main_test.go
@@ -1,34 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"strconv"
 )
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
 
 func TestMain(t *testing.T) {
 
-	assertCorrectMessage := func(t testing.TB, got, want string) {
-		t.Helper()
+	fmt.Println("TestMain")
+	testLanguages(t)
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+}
+
+func testLanguages(t *testing.T) {
+	languages := map[int]string{}
+	languages[english] = "Hello"
+	languages[spanish] = "Hola"
+	languages[french] = "Bonjour"
+
+	for key, value := range languages {
+			
+		t.Run("Language test for " + strconv.Itoa(key), func(t *testing.T) {
+			got := Hello("John", key)
+			want := value + ", John"
+
+			assertCorrectMessage(t, got, want)
+
+		})
 	}
-
-	t.Run("Hello man", func(t *testing.T) {
-		got := Hello("man")
-		want := "Hello, man"
-
-		assertCorrectMessage(t, got, want)
-
-	})
-
-	t.Run("Hello World when empty string passed", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
-
-		assertCorrectMessage(t, got, want)
-
-	})
 
 
 }
